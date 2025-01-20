9cc310722c0c28a25f92ec2b355fd6c42668de5c
henryno111 <h.agukwe@yahoo.com>
2025-01-20 12:11:10 +0100

    created multiple functional components

---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UsersPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Users</h1>
+            <p>List of users...</p>
+        </div>
+    );
+};
+
+export default UsersPage;
