ac858effd9b46767a944ae14c246cc2ea0e40ab6
henryno111 <h.agukwe@yahoo.com>
2025-01-20 15:07:55 +0100

    change map function

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from '@/components/layout/Header';
 import UserCard from '@/components/common/UserCard';
 
-const Users: React.FC<UserProps[]> = ( { users }) => {
+const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
     console.log(users)
     return (
         <div className="flex flex-col h-screen">
@@ -27,11 +27,11 @@ const Users: React.FC<UserProps[]> = ( { users }) => {
 
 export async function getStaticProps() {
     const response = await fetch("https://jsonplaceholder.typicode.com/users")
-    const posts = await response.json()
+    const users = await response.json()
   
     return {
       props: {
-        posts
+        users
       }
     }
   }
