f7ff212c3567d55b0604e2f92f8073c573343259
henryno111 <h.agukwe@yahoo.com>
2025-01-20 15:03:54 +0100

    debug push

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,11 +1,27 @@
+import { UserProps } from '@/interfaces';
 import React from 'react';
+import Header from '@/components/layout/Header';
+import UserCard from '@/components/common/UserCard';
 
-const Users: React.FC = () => {
+const Users: React.FC<UserProps[]> = ( { users }) => {
+    console.log(users)
     return (
-        <div>
-            <h1>Users</h1>
-            <p>List of users...</p>
-        </div>
+        <div className="flex flex-col h-screen">
+        <Header />
+        <main className="p-4">
+          <div className="flex justify-between">
+          <h1 className=" text-2xl font-semibold">Post Content</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+          </div>
+          <div className="grid grid-cols-3 gap-2 ">
+            {
+              users?.map(({ name, website, username, id, email, address, phone, company }: UserProps, key: number) => (
+                <UserCard name={name} website={website} username={username} id={id} email={email} address={address} phone={phone} company={company} key={key} />
+              ))
+            }
+          </div>
+        </main>
+      </div>
     );
 };
 
@@ -19,6 +35,6 @@ export async function getStaticProps() {
       }
     }
   }
-  
+
 export default Users;
 
